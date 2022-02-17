edc7b1343af0d5c6e5376bc9b05a8032b3c05fc2
kevinmcm1229 <kevinmcm1229@gmail.com>
2022-02-16 18:31:17 -0700

    Added register page and re-factored for material ui and new schema fields

---
+++ src/pages/Register.js
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import User from '../models/User';
+import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/Textfield';
+import { useFormControl } from '@mui/material/FormControl';
+
+const NewUser = () => {
+	const [firstname, setFirstName] = useState("");
+	const [lastname, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+
+	const nav = useNavigate();
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		User.register({firstname, lastname, email, password, nickname}).then((data) => {
+			console.log(data);
+			console.log(data.token);
+			// localStorage.setItem("uid", data.token)
+
+			if (data.status === 200) {
+				localStorage.setItem("uid", data.token);
+				nav("/profile");
+				window.location.reload();
+			}
+		})
+	}
+
+    return (
+		<>
+			<form className="userEntryForm" onSubmit={handleSubmit}>
+				<div className='form-group'>
+          <TextField id="outlined-basic" 
+            label="Outlined" 
+            variant="outlined" 
+            onChange={(e) => setFirstName(e.target.value)} 
+            value={firstname}
+          />
+				</div>
+				<div className='form-group'>
+          <TextField id="outlined-basic" 
+            label="Outlined" 
+            variant="outlined" 
+            onChange={(e) => setLastName(e.target.value)} 
+            value={lastname}
+          />
+				</div>
+        <div className='form-group'>
+          <TextField id="outlined-basic" 
+            label="Outlined" 
+            variant="outlined" 
+            onChange={(e) => setNickname(e.target.value)} 
+            value={nickname}
+          />
+				</div>
+        <div className='form-group'>
+          <TextField id="outlined-basic" 
+            label="Outlined" 
+            variant="outlined" 
+            onChange={(e) => setEmail(e.target.value)} 
+            value={email}
+          />
+				</div>
+				<div className='form-group'>
+        <TextField id="outlined-basic" 
+            label="Outlined" 
+            variant="outlined" 
+            onChange={(e) => setPassword(e.target.value)} 
+            value={password}
+          />
+				</div>
+        <Button type='submit' variant="outlined">Create Profile</Button>
+			</form>
+		</>
+	);
+}
+
+export default NewUser;
