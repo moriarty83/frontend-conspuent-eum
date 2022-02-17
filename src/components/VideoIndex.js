7350cf95ebec98e9e9f31230bab5215c3c3a77ef
kevinmcm1229 <kevinmcm1229@gmail.com>
2022-02-17 05:08:26 -0700

    Added video index route from backend and created VideoIndex component that is rendering data on Lesson.js testing page.

---
+++ src/components/VideoIndex.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { TextField, Button } from '@mui/material';
+import Video from "../models/Video";
+
+function VideoIndex(){
+    const [videos, setVideos] = useState([]);
+
+  useEffect(function() {
+      fetchVideos()
+  }, []);
+
+  function fetchVideos() {
+		Video.videoIndex().then((data) => {
+			console.log(data.videos);
+      setVideos(data.videos)
+		})
+	}
+
+  function generateVideoList(input) {
+    
+      console.log(input);
+      return (
+        <ul>
+          {input.map(video => {
+            return <li>{video.instructor}</li>
+          })}
+        </ul>
+      )
+    
+  }
+  console.log(typeof videos);
+
+    return(
+        <>
+          {generateVideoList(videos)}
+        </>
+    )
+}
+
+export default VideoIndex;
--- src/pages/Lesson.js
+++ src/pages/Lesson.js
@@ -6,6 +6,7 @@ import Reaction from '../components/Reaction';
 import { useParams } from "react-router-dom";
 import NewComment from '../components/NewComment';
 import EditComment from '../components/EditComment';
+import VideoIndex from '../components/VideoIndex';
 
 function Lesson (props){
     const { id } = useParams()
@@ -13,15 +14,13 @@ function Lesson (props){
         <>
             <h1>Lesson Page</h1>
             
-             // Jared section
             <Reaction videoId={ id } />
             {/* <Comment /> */}
-            // End Jared section
-      
-            // dev section
+
             <NewComment videoId={id} />
             <EditComment commentId="620dcf27c24c986e66836f0c" />
-            // end dev section
+
+            <VideoIndex />
         </>
     )
 }
