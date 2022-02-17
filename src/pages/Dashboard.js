42ad311eba22077eec1b5556cabd4d84e9fd1b7e
moriarty83 <moriarty83@gmail.com>
2022-02-17 10:14:12 -0500

    VideoShow case fix

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,10 +2,13 @@ import React, { useState,useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import {Routes, Route} from 'react-router-dom'
 
+import TableView from '../components/TableView'
+
 function Dashboard (props){
     return(
         <>
             <h1>Dashboard Page</h1>
+            <TableView />
         </>
     )
 }
