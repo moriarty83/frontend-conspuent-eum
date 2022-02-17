4826d9d7cbcbd9a4f71f8273682db0f4afe26627
moriarty83 <moriarty83@gmail.com>
2022-02-16 20:20:55 -0500

    routes

--- src/PageRouter.js
+++ src/PageRouter.js
@@ -1,16 +1,27 @@
 import React, { useState,useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import {Routes, Route} from 'react-router-dom'
+import Calculator from './Pages/Calculator';
+import CalculatorDash from './Pages/CalculatorDash';
+import CategoryDash from './Pages/CategoryDash';
 import Landing from './Pages/Landing';
+import Lesson from './Pages/Lesson';
+import LessonDash from './Pages/LessonDash';
 import Register from './Pages/Register';
 
 
 function PageRouter(props){
     return(
         <>
-            <h1>Router</h1>
             <Routes>
+                <Route path="/calculator/:name" element={<Calculator />} />
+                <Route path="/calculatordash" element={<CalculatorDash />} />
+
+                <Route path="/categorydash" element={<CategoryDash />} />
                 
+                <Route path="/lesson/:id" element={<Lesson />} />
+                <Route path="/lessondash" element={<LessonDash />} />
+
                 <Route path="/landing" element={<Landing />} />
                 <Route path="/register" element={<Register />} />
 
---
+++ src/Pages/LessonDash.js
@@ -0,0 +1,13 @@
+import React, { useState,useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom'
+
+function LessonDash (props){
+    return(
+        <>
+            <h1>Lesson Dash Page</h1>
+        </>
+    )
+}
+
+export default LessonDash
