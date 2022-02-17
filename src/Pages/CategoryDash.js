e7c5dbddb736d0e3d5867ebddcf4afe4f366e556
moriarty83 <moriarty83@gmail.com>
2022-02-16 21:14:01 -0500

    merge fix

--- src/Pages/CategoryDash.js
+++ src/Pages/CategoryDash.js
@@ -5,6 +5,9 @@ import {Routes, Route} from 'react-router-dom'
 function CategoryDash (props){
     const [category, setCategory] = useState()
 
+    // TODO: Function to get lessons in categor.
+
+
     return(
         <>
             <h1>Category Dash Page</h1>
