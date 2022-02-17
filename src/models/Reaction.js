02910be929e26505cede53eecb5571557a31a7a5
jg932 <jared.h.goldstein@gmail.com>
2022-02-16 23:12:03 -0500

    Reaction.js

---
+++ src/models/Reaction.js
@@ -0,0 +1,14 @@
+const url = 'http://localhost:4000/api/v1/';
+
+class Reaction {
+  static reaction = (data) => {
+    return fetch(`${url}/reactions`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${localStorage.uid}`
+        },
+        body: JSON.stringify(data),
+    }).then((res) => res.json());
+  }
+}
