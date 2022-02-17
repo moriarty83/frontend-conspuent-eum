46be4c78896d74dbecc95c35acf94f8310750854
moriarty83 <moriarty83@gmail.com>
2022-02-17 00:20:43 -0500

    comment delete added

--- src/models/Comments.js
+++ src/models/Comments.js
@@ -13,6 +13,29 @@ class Comments{
           body: JSON.stringify(data),
       }).then((res) => res.json());
     }
+
+    static editComment = (data, id) => {
+      console.log(data)
+      console.log(`${url + "/" + id}`)
+      return fetch(`${url + "/" + id}`, {
+          method: "PUT",
+          headers: {
+                authorization: `Bearer ${localStorage.uid}`,
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+      }).then((res) => res.json());
+    }
+
+    static deleteComment = (id) => {
+      return fetch(`${url + "/" + id}`, {
+          method: "DELETE",
+          headers: {
+                authorization: `Bearer ${localStorage.uid}`,
+              "Content-Type": "application/json",
+          },
+      }).then((res) => res.json());
+    }
 }
 
 export default Comments
